ad100b813b5b8ca0251c2bc099d2d39fb6315f45
Gaebel, Sebastian M <sebastian.gaebel@nextevolution.de>
2023-10-02 17:49:54 +0200

    Bumps version for new feature release.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ polygons.
 """
 
 setup (name = 'polygon_neighbours',
-       version = '1.0',
+       version = '1.1',
        description = 'CPython extension for finding bordering polygons.',
        url = 'https://github.com/sgaebel/polygon_neighbours',
        author = 'Dr. Sebastian M. Gaebel',
