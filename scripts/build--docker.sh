d9f7ad06a5a51d7048256381eb4422e49419a734
Gaebel, Sebastian M <sebastian.gaebel@nextevolution.de>
2023-10-04 14:41:54 +0200

    Fixes minor error in build script.

--- scripts/build--docker.sh
+++ scripts/build--docker.sh
@@ -1,6 +1,6 @@
 python3.10 -m build --outdir /dist
 python3.10 -m build --sdist --outdir /dist
-auditwheel repair /output/*whl -w /dist
+auditwheel repair /dist/*whl -w /dist
 yes | rm /dist/*-linux_*
 echo "Waiting to allow for output being copied to host."
 sleep 30
